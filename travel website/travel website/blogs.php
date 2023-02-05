cb3ff3ff6148795046cd457b89c984800c673f9d
pratik259 <68510405+pratik259@users.noreply.github.com>
2023-02-05 10:35:04 +0530

    Add files via upload

---
+++ travel website/travel website/blogs.php
@@ -0,0 +1,175 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">Travel Blazers Pvt Ltd...</a>
+
+   <nav class="navbar">
+      <a href="home.php">HOME</a>
+      <a href="about.php">ABOUT</a>
+      <a href="package.php">PACKAGE</a>
+      <a href="book.php">BOOK</a>
+      <a href="blogs.php">BLOGS</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+
+
+<!-- blogs section starts  -->
+
+<section class="blogs" id="blogs">
+
+   <h3 class="heading-title"> Our daily posts</h3>
+
+   <div class="blogs-slider">
+
+      <div class="wrapper">
+
+      <div class="slide">
+         <img src="images/img-1.jpg" alt="">
+         <div class="icons">
+            <a href="#"> <i class="fas fa-calendar"></i> 21st may,2022</a>
+            <a href="#"> <i class="fas fa-user"></i> by admin</a>
+     </div>
+     <h2>blog title goes here.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit incidunt deleniti quos quibusdam a voluptatem fugit tempore officia doloribus aliquam.</p>
+    <a href="#" class="btn">read more</a>
+    </div><br><br>
+    <div class="slide">
+         <img src="images/img-2.jpg" alt="">
+         <div class="icons">
+            <a href="#"> <i class="fas fa-calendar"></i> 21st may,2022</a>
+            <a href="#"> <i class="fas fa-user"></i> by admin</a>
+     </div>
+     <h2>blog title goes here.</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit esse reiciendis quo voluptatem nemo. Cum sit voluptatibus eos minima nihil.</p>
+    <a href="#" class="btn">read more</a>
+    </div><br><br>
+
+    <div class="slide">
+         <img src="images/img-3.jpg" alt="">
+         <div class="icons">
+            <a href="#"> <i class="fas fa-calendar"></i> 21st may,2022</a>
+            <a href="#"> <i class="fas fa-user"></i> by admin</a>
+     </div>
+     <h2>blog title goes here.</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A eos natus neque facere, odit doloremque assumenda voluptatum officia dignissimos vero.</p>
+    <a href="#" class="btn">read more</a>
+    </div><br><br>
+
+    <div class="slide">
+         <img src="images/img-4.jpg" alt="">
+         <div class="icons">
+            <a href="#"> <i class="fas fa-calendar"></i> 21st may,2022</a>
+            <a href="#"> <i class="fas fa-user"></i> by admin</a>
+     </div>
+     <h2>blog title goes here.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsam minima facere repellat dicta quae numquam provident eos eaque eum.</p>
+    <a href="#" class="btn">read more</a>
+    </div><br><br>
+
+    
+    <div class="slide">
+         <img src="images/img-5.jpg" alt="">
+         <div class="icons">
+            <a href="#"> <i class="fas fa-calendar"></i> 21st may,2022</a>
+            <a href="#"> <i class="fas fa-user"></i> by admin</a>
+     </div>
+     <h2>blog title goes here.</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum quae non laborum reiciendis dolorem nam maxime nesciunt porro architecto numquam!</p>
+    <a href="#" class="btn">read more</a>
+    </div>
+
+
+         
+      </div>
+
+   </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> HOME</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> ABOUT</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> PACKAGE</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> BOOK</a>
+         <a href="blogs.php"> <i class="fas fa-angle-right"></i> BLOGS</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> 123456789 </a>
+         <a href="#"> <i class="fas fa-phone"></i> 987654321 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> pratiksurti@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 401305 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+
+</section>
+
+<!-- footer section ends -->
---
+++ travel website/travel website/package.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">Travel Blazers Pvt Ltd...</a>
+
+   <nav class="navbar">
+      <a href="home.php">HOME</a>
+      <a href="about.php">ABOUT</a>
+      <a href="package.php">PACKAGE</a>
+      <a href="book.php">BOOK</a>
+      <a href="blogs.php">BLOGS</a>
+      
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>One of the Seven Wonders of the World, the Taj Mahal in Agra is among the most visited monuments on the planet.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Andaman Nicobar Islands is famous for its absolutely stunning beaches, rich biodiversity, beautiful marine ecosystem and tropical evergreen forests teeming with wildlife.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>enhance the natural beauty of the environment, thus helping to develop tourism and provide recreation facilities.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>visit these place basically it is famoush for because we got here to watch the different holy places and especially find beautiful scenic beauty with interesting weather and peacefull enviroment </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Manali is a rustic enclave known for its cool climate and snow-capped mountains, offering respite to tourists escaping scorching heat of the plains. mainly because of its natural bounties and salubrious climate.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>The state of Goa, in India, is famous for its beaches and places of worship. Tourism is its primary industry, and is generally focused on the coastal areas of Goa, with decreased tourist activity inland.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>visit these place to watch the holy places and find beautiful , peacefull enviroment  </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> HOME</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> ABOUT</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> PACKAGE</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> BOOK</a>
+         <a href="blogs.php"> <i class="fas fa-angle-right"></i>BLOGS </a>
+         
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> 123456789 </a>
+         <a href="#"> <i class="fas fa-phone"></i> 987654321 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> pratiksurti@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 401305 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>  
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
